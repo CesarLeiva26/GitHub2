6647a00e36cfeec2fb8552a35fd1ce59d8f93162
CesarLeiva26 <cesaremanuelleivajuro@gmail.com>
2023-12-04 19:26:47 -0500

    Proyecto Git Hub 2

---
+++ src/test/java/youtube/com/pe/GitHub2/GitHub2ApplicationTests.java
@@ -0,0 +1,13 @@
+package youtube.com.pe.GitHub2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHub2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
